7437dbdfaae224b1165c1866aab722a8d832ce45
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-06-15 10:54:32 +0300

    feat: create Local Storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,21 @@
-const useLocalStorage = () => {
+import { useEffect, useState } from 'react';
 
-}
+const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [key, storedValue]);
+
+  const updateStoredValue = (value: T) => {
+    setStoredValue(value);
+  };
+
+  return [storedValue, updateStoredValue];
+};
 
 export {
   useLocalStorage
